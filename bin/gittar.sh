2a0aef7ca1e3f95c84e4d0ce09427b5cd9c01cb0
Jason Dusek <jason.dusek@gmail.com>
2010-05-06 17:03:51 -0700

    Numerous improvements due to use of project at work.

--- bin/gittar.sh
+++ bin/gittar.sh
@@ -2,12 +2,23 @@
 
 usage() {
 cat <<USAGE
- USAGE: $0 ( name | tgz | tbz )
+ USAGE: gittar name
+        gittar ( tar | tgz | tbz | tar.gz | tar.bz2 )
 
-  Prepare a tarball of the repository with the name set by the latest tag and
-  the number of revisions since that tag.
+  In the first form, output the name the package would be assigned. In second
+  form, prepare an archive of the package in the desired format.
 
 USAGE
+}
+
+archiver() {
+  case "$1" in
+    tar)            echo cat ;;
+    tbz)            echo bzip2 ;;
+    tar.bz2)        echo bzip2 ;;
+    tgz)            echo gzip ;;
+    tar.gz)         echo gzip ;;
+  esac
 }
 
 root() {
@@ -17,34 +28,33 @@ root() {
   pwd
 }
 
+version() {
+  v="-`git describe`"
+  [ $? = 0 ] && echo "$v"
+}
+
 name() {
   r=`root`
-  v=`git describe`
   project_name=`basename "$r"`
-  echo "$project_name-$v"
+  echo "$project_name`version 2>/dev/null`"
 }
 
 archive() {
   r=`root`
   n=`name`
   t="$n.$1"
-  case "$1" in
-    tbz)            archiver=bzip2;;
-    tgz)            archiver=gzip;;
-  esac
-  echo "Creating archive:" 1>&2
+  echo "Creating archive." 1>&2
   echo $t
-  git archive HEAD --prefix="$n"/ | $archiver > $t
+  git archive HEAD --prefix="$n"/ | `archiver $1` > $t
 }
 
 
 case "$1" in
-  -h|-?|--help)     usage ; exit 0 ;;
+  -h|'-?'|--help)   usage ; exit 0 ;;
   name)             name ;;
-  tgz|tbz)          archive $1 ;;
-  *)                (echo "Input error." ; usage) 1>&2 ; exit 2 ;;
+  *)                case `archiver "$1"` in
+                      '')       (echo "Input error." ; usage) 1>&2 ; exit 2 ;;
+                      *)        archive $1 ;;
+                    esac ;;
 esac
-
-
-
 
